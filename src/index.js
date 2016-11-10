55bb825763048d01a33c7a2f0b6c16fe5ab7810f
Vitaly Blinovskov <blinovskov@yandex-team.ru>
2016-11-10 17:29:08 +0500

    Revert "build(deps): user unbuilt version of localforage"
    This reverts commit 4684f8736d6271c77e126ed9a42e43b143dc5482.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import factory from './factory';
-import localforage from 'localforage/src/localforage';
+import localforage from 'localforage/dist/localforage.nopromises';
 
 export default factory(localforage);
